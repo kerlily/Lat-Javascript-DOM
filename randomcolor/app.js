859d4e877794672fdddd9d775124f154d84f84ca
kerlily <farhan15062004@gmail.com>
2024-12-21 17:40:17 +0700

    membuat background color random generator

--- randomcolor/app.js
+++ randomcolor/app.js
@@ -2,12 +2,13 @@ const button = document.querySelector('button');
 const h1 = document.querySelector('h1');
 
 button.addEventListener('click', () => {
-	const newColor = generateRandomColor();
-	document.body.style.backgroundColor = newColor;
-	h1.innerText = newColor;
+	const newColor = generatorColor();
+	document.body.style.backgroundColor = newColor
+	console.log(newColor);
+	h1.innerText = document.body;
 });
 
-const generateRandomColor = () => {
+const generatorColor = () => {
 	const r = Math.floor(Math.random() * 255);
 	const g = Math.floor(Math.random() * 255);
 	const b = Math.floor(Math.random() * 255);
