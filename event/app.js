9af3e9efa4fbd908b49a07ca98845c6159d0a622
kerlily <farhan15062004@gmail.com>
2024-12-21 17:27:57 +0700

    belajar event listener

--- event/app.js
+++ event/app.js
@@ -9,3 +9,16 @@ const scream = () => {
 }
 
 button.onmouseover = scream
+
+
+const eventbtn = document.querySelector('#eventbtn');
+eventbtn.addEventListener('dblclick', step2);
+eventbtn.addEventListener('click', step1);
+
+function step1() {
+	console.log('step 1');
+}
+
+function step2() {	
+	console.log('step 2');
+}
