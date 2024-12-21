b3a52ea06421018a870eb82bdc79ae1428984b41
kerlily <farhan15062004@gmail.com>
2024-12-21 11:54:48 +0700

    belajar event dasar

--- event/app.js
+++ event/app.js
@@ -1,26 +1,11 @@
 const button = document.querySelector('#clickme');
 
 button.onclick = function () {
-	console.log('You clicked me');
+	console.log('bdhkajjbdio');
 };
 
 const scream = () => {
-	console.log('Dont touch me');
-};
-
-button.onmouseover = scream;
-
-const eventbtn = document.querySelector('#eventbtn');
-eventbtn.addEventListener('click', stepSatu);
-eventbtn.addEventListener('click', stepDua);
-
-function stepSatu() {
-	console.log('step satu');
+	console.log('bdhkajjbdio');
 }
 
-function stepDua() {
-	console.log('step dua');
-}
-
-// eventbtn.onclick = stepSatu;
-// eventbtn.onclick = stepDua;
+button.onmouseover = scream
---
+++ segitga.js
@@ -0,0 +1,10 @@
+// *
+// ***
+// *****
+// *******
+// *********
+
+for(i=1; i<=10; i = i + 2 ) {
+    const bintang = '*'.repeat(i);
+    console.log(bintang);
+}
