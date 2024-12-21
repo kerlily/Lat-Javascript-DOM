0cf9da1b6ac02e5fa9b8b7885c54220c6a1f4efa
kerlily <farhan15062004@gmail.com>
2024-12-22 00:53:49 +0700

    membuat event object

--- eventobject/app.js
+++ eventobject/app.js
@@ -4,21 +4,13 @@ document.querySelector('button').addEventListener('click', (e) => {
 
 const input = document.querySelector('input');
 input.addEventListener('keydown', (e) => {
-	switch (e.code) {
-		case 'ArrowUp':
-			console.log('Tombol Arah Atas');
+	switch(e.code){
+		case 'Enter':
+			console.log('Enter');
 			break;
-		case 'ArrowDown':
-			console.log('Tombol Arah Bawah');
-			break;
-		case 'ArrowLeft':
-			console.log('Tombol Arah Kiri');
-			break;
-		case 'ArrowRight':
-			console.log('Tombol Arah Kanan');
-			break;
-		default:
-			console.log('diabaikan');
+			case 'ArrowUp':
+				console.log('atas');
+				break;
 	}
 });
 
